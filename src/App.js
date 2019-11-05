cd828a4740844fec043cbc43112018e73672c81b
Aid Thompson <thePandaRiot@gmail.com>
2019-11-05 15:46:17 +0000

    BLAH

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>this is a dummy app</h1>
+      <h1>this is a dummy app</h1>
+      <p>I just love my teddies</p>
+      <p>this is one</p>
+      <p>this is two</p>
+      <p>this is three</p>
     </div>
   );
 }
